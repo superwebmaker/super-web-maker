6b1bdde16d2183fbc08cc94bd8d0e46bb852e3b1
Elf-mousE <ifmouz@gmail.com>
2020-04-03 10:18:15 +0800

    update

--- config/balm.after-task.js
+++ config/balm.after-task.js
@@ -1,19 +1,22 @@
+const balm = require('balm');
 const { spawn } = require('child_process');
 
 const superWebMakerServer = () => {
-  const egg = spawn('./node_modules/.bin/egg-bin', ['dev']);
+  if (balm.config.env.isDev) {
+    const egg = spawn('./node_modules/.bin/egg-bin', ['dev']);
 
-  egg.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
+    egg.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
 
-  egg.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-  });
+    egg.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+    });
 
-  egg.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
+    egg.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  }
 };
 
 module.exports = superWebMakerServer;
